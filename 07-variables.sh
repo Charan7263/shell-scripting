106bf7e4d73594a888981a0177cc1e04bd02d17f
Charan A <charan@Charans-MacBook-Air.local>
2026-05-26 11:55:55 +0530

    variables  added

---
+++ 07-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "enter your username:"
+read  USERNAME # -s flag is used to hide the input while typing
+
+echo "your username is : $USERNAME"
+
+echo "entern your password"
+
+read -s PASSWORD
