3afd2848a00a393e36efc17535b72d111da184dd
Charan A <charan@Charans-MacBook-Air.local>
2026-05-28 17:27:54 +0530

    shell scripting

---
+++ 10-special-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "all variables: $@"
+echo "number of variables: $#"
+echo "present working directory : $PWD"
+echo "home directory of current user:$HOME"
+echo "script name: $0"
+echo "which user is running this script: $USER"
+echo "processs id of current script: $$"
+echo "process of last  of last command in backend: $!"
+ 
