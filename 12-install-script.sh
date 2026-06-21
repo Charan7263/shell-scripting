cf982df96bdc64cdde21654eb7589b64ee6145ac
Charan A <charan@Charans-MacBook-Air.local>
2026-06-21 23:16:36 +0530

    files added

--- 12-install-script.sh
+++ 12-install-script.sh
@@ -8,12 +8,18 @@ then
     exit 1
 fi
 
-dnf install mysql -y
+dnf list installed mysql
 
 if [ $? -ne 0 ]
 then 
-    echo "mysql installation failure"
-    exit 1
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "mysql installation failure"
+        exit 1 
+    else
+        echo "mysql installation success"
+    fi
 else
-    echo "mysql installation success"
+    echo "mysql is already installed"
 fi
